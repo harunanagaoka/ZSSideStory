94440beae55dcfa54bfaa3bbc010cbfedb49567f
harunanagaoka <shinkaigyo428@i.softbank.jp>
2025-03-23 16:00:27 +0900

    [Add]アイテム取得用メソッド追加

---
+++ ZSSideStory/Assets/Scripts/ItemGet.cs
@@ -0,0 +1,38 @@
+using Fungus;
+using UnityEngine;
+
+public class ItemGet : MonoBehaviour
+{
+    [SerializeField] private Flowchart m_flowChart;
+    private string m_ItemVariable = "Item";
+
+    public enum ItemName
+    {
+        TestItem,
+        TestItem2,
+        TestItem3,
+        ItemCount
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// ランダムなアイテムを選び取得
+    /// </summary>
+    void SetRandomItem()
+    {
+        int randomItem = Random.Range(0, (int)ItemName.ItemCount);
+        ItemName name = (ItemName)randomItem;
+        string ItemValue = name.ToString();
+        m_flowChart.SetStringVariable(m_ItemVariable, ItemValue);
+    }
+}
